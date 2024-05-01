fa487c8c73d238f04ff8b7e9033c14c7e491f2e9
William Allen <williamallen933@gmail.com>
2024-05-01 16:56:12 -0400

    created variable's for tthe eements in html upunder the Registration form

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const regForm = document.getElementById
+('registration');
+const username = document.getElementsByClassName["username"];
+const email = document.getElementsByClassName["email"];
+const password = document.getElementsByClassName["password"];
+const passwordCheck = document.getElementsByClassName["passwordCheck"];
+console.log('regForm');
+
+
