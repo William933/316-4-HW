dd8d7b328a80c43a87079007710656fd2f25dc1f
William Allen <williamallen933@gmail.com>
2024-05-01 23:51:23 -0400

    added RegEx and require to the html and js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const username = document.getElementsByClassName["username"];
 const email = document.getElementsByClassName["email"];
 const password = document.getElementsByClassName["password"];
 const passwordCheck = document.getElementsByClassName["passwordCheck"];
+const registr = document.getElementsByClassName["Registration"]
 console.log('regForm');
-
-
+const re = /^(?!\s*$).+/;
+const re2 = /ab+c/
